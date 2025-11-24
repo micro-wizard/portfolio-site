c3ea04e1b0bf520d717e42c8fd573290686d20bc
Nathan Spelts <nathan@spelts.net>
2025-11-23 17:45:13 -0800

    work and education experience autogened into html

--- resume/generate.py
+++ resume/generate.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 import json
+from datetime import datetime
 
 
 def format_link_to_latex(link: str) -> str:
@@ -23,6 +24,22 @@ def date_range_to_latex(
     return ""
 
 
+def date_range_to_html(
+    start_year: str | bool,
+    start_month: str | bool,
+    end_year: str | bool,
+    end_month: str | bool,
+    no_start_preamble: str = "Graduated",
+) -> str:
+    if not start_year and end_year:
+        return f"{no_start_preamble} {datetime(int(end_year), int(end_month), 1).strftime('%B %Y')}"
+    if start_year and not end_year:
+        return f"{datetime(int(start_year), int(start_month), 1).strftime('%B %Y')} - Present"
+    if start_year and end_year:
+        return f"{datetime(int(start_year), int(start_month), 1).strftime('%B %Y')} - {datetime(int(end_year), int(end_month), 1).strftime('%B %Y')}"
+    return ""
+
+
 def create_latex_section(title: str) -> str:
     return f"\\Section{{{title}}}{{{title}}}{{PDF:{title}}}"
 
@@ -41,7 +58,7 @@ with open("tex_src/default.json", "r") as f:
     data = json.load(f)
 
 
-subtitle = env.get_template("tex_src/subtitle.tex.tpl").render(
+latex_subtitle = env.get_template("tex_src/subtitle.tex.tpl").render(
     phone_number=data["subtitle"]["phone_number"],
     email=data["subtitle"]["email"],
     location=data["subtitle"]["location"],
@@ -49,68 +66,115 @@ subtitle = env.get_template("tex_src/subtitle.tex.tpl").render(
         [format_link_to_latex(link) for link in data["subtitle"]["links"]]
     ),
 )
+html_feature_template = env.get_template("tsx_src/feature_section.tsx.tpl")
 
 if data["summary"] != "":
-    summary = create_latex_section("Summary")
-    summary += data["summary"]
+    latex_summary = create_latex_section("Summary")
+    latex_summary += data["summary"]
 else:
-    summary = ""
+    latex_summary = ""
 
-if len(data["jobs"]) > 0:
-    work = create_latex_section("Work Experience")
-else:
-    work = ""
-work_template = env.get_template("tex_src/work.tex.tpl")
-work += "\n".join(
-    work_template.render(
-        company=job["company"],
-        url=job["url"],
-        location=job["location"],
-        dates=date_range_to_latex(
-            job["start_year"], job["start_month"], job["end_year"], job["end_month"]
-        ),
-        job_title=job["job_title"],
-        bullet_points="".join(
-            [f"\\SubBulletItem\n{line}\n" for line in job["bullet_points"]]
+jobs = data["jobs"]
+if len(jobs) > 0:
+    latex_work_template = env.get_template("tex_src/work.tex.tpl")
+    latex_work = create_latex_section("Work Experience")
+    latex_work += "\n".join(
+        latex_work_template.render(
+            company=job["company"],
+            url=job["url"],
+            location=job["location"],
+            dates=date_range_to_latex(
+                job["start_year"], job["start_month"], job["end_year"], job["end_month"]
+            ),
+            job_title=job["job_title"],
+            bullet_points="".join(
+                [f"\\SubBulletItem\n{line}\n" for line in job["bullet_points"]]
+            ),
+        )
+        for job in jobs
+    )
+    html_work_template = env.get_template("tsx_src/job.html.tpl")
+    html_work = html_feature_template.render(
+        title="Work Experience",
+        svg=data["jobs_svg"],
+        html="\n".join(
+            html_work_template.render(
+                title=job["job_title"],
+                company=job["company"],
+                employment_length=date_range_to_html(
+                    job["start_year"],
+                    job["start_month"],
+                    job["end_year"],
+                    job["end_month"],
+                ),
+                bullet_points="".join(
+                    [f"<li>{line}</li>" for line in job["bullet_points"]]
+                ),
+            )
+            for job in jobs
         ),
     )
-    for job in data["jobs"]
-)
-
-if len(data["degrees"]) > 0:
-    education = create_latex_section("Education")
 else:
-    education = ""
-education_template = env.get_template("tex_src/education.tex.tpl")
-education += "\n".join(
-    education_template.render(
-        school=degree["school"],
-        url=degree["url"],
-        location=degree["location"],
-        dates=date_range_to_latex(
-            degree["start_year"],
-            degree["start_month"],
-            degree["end_year"],
-            degree["end_month"],
-        ),
-        degree_name=degree["degree_name"],
-        degree_specialization=f" -- \\textit{{{degree['degree_specialization']}}}"
-        if degree["degree_specialization"]
-        else "",
-        bullet_points="".join(
-            [f"\\SubBulletItem\n{line}\n" for line in degree["bullet_points"]]
+    latex_work = ""
+    html_work = ""
+
+degrees = data["degrees"]
+if len(degrees) > 0:
+    latex_education = create_latex_section("Education")
+    latex_education_template = env.get_template("tex_src/education.tex.tpl")
+    latex_education += "\n".join(
+        latex_education_template.render(
+            school=degree["school"],
+            url=degree["url"],
+            location=degree["location"],
+            dates=date_range_to_latex(
+                degree["start_year"],
+                degree["start_month"],
+                degree["end_year"],
+                degree["end_month"],
+            ),
+            degree_name=degree["degree_name"],
+            degree_specialization=f" -- \\textit{{{degree['degree_specialization']}}}"
+            if degree["degree_specialization"]
+            else "",
+            bullet_points="".join(
+                [f"\\SubBulletItem\n{line}\n" for line in degree["bullet_points"]]
+            ),
+        )
+        for degree in degrees
+    )
+    html_education_template = env.get_template("tsx_src/education.html.tpl")
+    html_education = html_feature_template.render(
+        title="Education",
+        svg=data["education_svg"],
+        html="\n".join(
+            html_education_template.render(
+                degree=degree["degree_name"],
+                school=degree["school"],
+                dates=date_range_to_html(
+                    degree["start_year"],
+                    degree["start_month"],
+                    degree["end_year"],
+                    degree["end_month"],
+                ),
+                bullet_points="".join(
+                    [f"<li>{line}</li>" for line in degree["bullet_points"]]
+                ),
+            )
+            for degree in degrees
         ),
     )
-    for degree in data["degrees"]
-)
+else:
+    latex_education = ""
+    html_education = ""
 
 if len(data["projects"]) > 0:
-    projects = create_latex_section("Projects")
+    latex_projects = create_latex_section("Projects")
 else:
-    projects = ""
-projects_template = env.get_template("tex_src/projects.tex.tpl")
-projects += "\n".join(
-    projects_template.render(
+    latex_projects = ""
+latex_projects_template = env.get_template("tex_src/projects.tex.tpl")
+latex_projects += "\n".join(
+    latex_projects_template.render(
         name=project["name"],
         overview=project["overview"],
         link=format_link_to_latex(project["url"]),
@@ -122,12 +186,12 @@ projects += "\n".join(
 )
 
 if len(data["skills"]) > 0:
-    skills = create_latex_section("Skills")
+    latex_skills = create_latex_section("Skills")
 else:
-    skills = ""
-skills_template = env.get_template("tex_src/skills.tex.tpl")
-skills += "\n".join(
-    skills_template.render(
+    latex_skills = ""
+latex_skills_template = env.get_template("tex_src/skills.tex.tpl")
+latex_skills += "\n".join(
+    latex_skills_template.render(
         name=skill["name"],
         items=", ".join([line for line in skill["items"]]),
     )
@@ -141,11 +205,18 @@ with open("tex_src/nathanSpeltsResume.tex", "w") as out:
             name=data["name"],
             pdf_title=data["pdf_title"],
             website=data["website"],
-            subtitle=subtitle,
-            summary=summary,
-            work=work,
-            education=education,
-            projects=projects,
-            skills=skills,
+            subtitle=latex_subtitle,
+            summary=latex_summary,
+            work=latex_work,
+            education=latex_education,
+            projects=latex_projects,
+            skills=latex_skills,
+        )
+    )
+
+with open("tsx_src/index.tsx", "w") as out:
+    out.writelines(
+        env.get_template("tsx_src/index.tsx.tpl").render(
+            jobs=html_work, education=html_education
         )
     )
---
+++ resume/tsx_src/education.html.tpl
@@ -0,0 +1,15 @@
+<h2>
+  <strong>(((degree)))</strong>
+</h2>
+<h4>
+  <em>(((school)))</em>
+</h4>
+<ul>
+  <li><strong>Attendance:</strong> (((dates)))</li>
+  <li>
+    <strong>Details:</strong>
+    <ul>
+      (((bullet_points)))
+    </ul>
+  </li>
+</ul>
